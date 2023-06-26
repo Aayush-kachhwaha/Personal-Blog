31b431465fc68259db24c16b6302b36d0dc6578c
Aayush Kachhwaha <aayushk2609@gmail.com>
2023-06-26 16:27:54 +0530

    up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Nav from './Component/Nav/Nav.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './nav.css';
+import ViewHeadlineIcon from '@mui/icons-material/ViewHeadline';
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <a href="#" ><ViewHeadlineIcon/></a>
+    </div>
+  )
+}
+
+export default Nav
